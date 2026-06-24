994fbcd9e64a736a89941133e2384cb8eb1883d8
Rajputsourav <113873826+Rajputsourav@users.noreply.github.com>
2026-06-24 19:10:15 +0545

    Time: 0 ms (100.00%) | Memory: 31 MB (59.90%) - LeetSync

---
+++ 238-product-of-array-except-self/product-of-array-except-self.c
@@ -0,0 +1,23 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* productExceptSelf(int* nums, int numsSize, int* returnSize) {
+    *returnSize = numsSize;
+
+    int *result = (int*)malloc(numsSize * sizeof(int));
+
+
+    result[0] = 1;
+    for (int i = 1; i < numsSize; i++) {
+        result[i] = result[i - 1] * nums[i - 1];
+    }
+
+ 
+    int suffix = 1;
+    for (int i = numsSize - 1; i >= 0; i--) {
+        result[i] *= suffix;
+        suffix *= nums[i];
+    }
+
+    return result;
+}
