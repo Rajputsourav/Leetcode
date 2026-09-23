494da47e0889ab33207e1526cf8618f50a7aa1b2
Sourav Kumar Singh <113873826+Rajputsourav@users.noreply.github.com>
2026-09-23 21:03:46 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (12.54%) - LeetSync

---
+++ 605-can-place-flowers/can-place-flowers.c
@@ -0,0 +1,18 @@
+#include <stdbool.h>
+bool canPlaceFlowers(int* flowerbed, int flowerbedSize, int n) {
+    if (n == 0) return true;
+    
+    for (int i = 0; i < flowerbedSize; i++) {
+        if (flowerbed[i] == 0 && 
+            (i == 0 || flowerbed[i - 1] == 0) && 
+            (i == flowerbedSize - 1 || flowerbed[i + 1] == 0)) {
+            
+            flowerbed[i] = 1; 
+            n--;              
+            
+            if (n == 0) return true; 
+        }
+    }
+    
+    return n <= 0;
+}
